6ac9ed3ba40d90ca8175d29af5d3c1a3e0de7750
frikh maissane <frikhmaissane@MacBook-Air-de-frikh.local>
2024-03-13 16:06:22 +0100

    création du tirage

---
+++ td01.py
@@ -0,0 +1,9 @@
+###programationTD1###
+#exercice1#
+import numpy as np
+
+def scrable(n):
+    alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for i in range (n):
+        tirage.append(alphabet[random.randrange(1,n,1)])
+
